687eff9acb040e3dbf847a6e929f738b6c30b357
Tomas Skogberg <tomas.skogberg@gmail.com>
2017-11-27 15:19:34 +0100

    Remove scaffolding tests

--- test/ex_gridhook_web/views/layout_view_test.exs
+++
@@ -1,3 +0,0 @@
-defmodule ExGridhookWeb.LayoutViewTest do
-  use ExGridhookWeb.ConnCase, async: true
-end
--- test/ex_gridhook_web/views/page_view_test.exs
+++
@@ -1,3 +0,0 @@
-defmodule ExGridhookWeb.PageViewTest do
-  use ExGridhookWeb.ConnCase, async: true
-end

153d8caff74f995df0e06669fd29d8d2cea2058f
Tomas Skogberg <tomas.skogberg@gmail.com>
2017-11-29 16:16:19 +0100

    Cleanup
    Removing redundant code and fix deprication warnings.

--- test/ex_gridhook_web/controllers/root_controller_test.exs
+++ test/ex_gridhook_web/controllers/root_controller_test.exs
@@ -1,20 +1,21 @@
 defmodule ExGridhookWeb.RootControllerTest do
   use ExGridhookWeb.ConnCase
 
-  test "GET /", %{conn: conn} do
-    conn = get(conn, "/")
+  test "GET /" do
+    conn = get(build_conn(), "/")
+
     assert response(conn, 200) =~ "handles incoming sendgrid events"
   end
 
-  test "GET /revision without ENV set", %{conn: conn} do
-    conn = get(conn, "/revision")
+  test "GET /revision without ENV set" do
+    conn = get(build_conn(), "/revision")
     assert response(conn, 200) =~ "no revision is set."
   end
 
-  test "GET /revision with ENV set", %{conn: conn} do
+  test "GET /revision with ENV set" do
     System.put_env("GIT_COMMIT", "12345")
 
-    conn = get(conn, "/revision")
+    conn = get(build_conn(), "/revision")
     assert response(conn, 200) == "12345"
 
     System.delete_env("GIT_COMMIT")

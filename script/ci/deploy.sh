f446018c77e5a55b7a7f85939ae2d34c4eadbac6
Tomas Skogberg <tomas.skogberg@gmail.com>
2018-01-16 16:48:42 +0100

    CircleCI: Heroku deploy
    Re-using what we have from other projects. Hopefully it just works.

---
+++ script/ci/deploy.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -e
+
+# Add config for heroku commands and git based deploy over https
+echo -e "machine api.heroku.com\n  login $HEROKU_API_USER\n  password $HEROKU_API_TOKEN\nmachine code.heroku.com\n  login $HEROKU_API_USER\n  password $HEROKU_API_TOKEN\nmachine git.heroku.com\n  login $HEROKU_API_USER\n  password $HEROKU_API_TOKEN" > ~/.netrc
+chmod 0600 ~/.netrc
+
+cd ~/$CIRCLE_PROJECT_REPONAME
+
+# Deploy
+revision=$(git rev-parse HEAD)
+app_name=$1
+
+function _main {
+  _deploy_to_heroku
+  _smoke_test
+}
+
+function _deploy_to_heroku {
+  heroku git:remote --app $app_name
+
+  # Workaround for https://github.com/travis-ci/dpl/issues/127#issuecomment-42397378
+  set +e
+  git fetch --unshallow 2> /dev/null
+  set -e
+
+  git push heroku master
+}
+
+function _smoke_test {
+  ruby script/ci/support/wait_for_new_revision_to_serve_requests.rb $app_name $revision
+
+  echo
+  echo "Running smoke test."
+
+  APP_URL=https://$app_name.herokuapp.com script/ci/smoke_test.sh
+}
+
+_main

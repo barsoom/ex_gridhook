8f7238651c5448a7738579f278bb8318880fa4ed
Henrik Nyh <henrik@nyh.se>
2020-01-23 10:19:27 +0100

    Doc insights into JSON-in-YAML
    Does @tskogberg know more?

--- lib/ex_gridhook/yaml_type.ex
+++ lib/ex_gridhook/yaml_type.ex
@@ -5,5 +5,9 @@ defmodule ExGridhook.YamlType do
 
   def cast(any), do: {:ok, any}
   def load(value), do: {:ok, YamlElixir.read_from_string!(value)}
+
+  # Henrik's and Albert's best guess 2020-01-23 on why we put JSON among YAML:
+  # we only use this for arrays and hashes where it seems YAML can decode JSON successfully,
+  # e.g.: YAML.load(JSON.dump({"url" => "hi"}))
   def dump(value), do: {:ok, "---\n" <> Jason.encode!(value)}
 end

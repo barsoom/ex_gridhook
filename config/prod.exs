26d66ca5659dfc5b776e91d44ab00ec5ff66f070
Tomas Skogberg <tomas.skogberg@gmail.com>
2021-04-23 10:22:38 +0200

    Enable heroku postgres connection pooling
    Enabled for gridlook also.
    
    https://github.com/elixir-ecto/postgrex/pull/156
    https://devcenter.heroku.com/articles/postgres-connection-pooling

--- config/prod.exs
+++ config/prod.exs
@@ -25,6 +25,7 @@ config :logger, level: :debug
 # Configure the database
 config :ex_gridhook, ExGridhook.Repo,
   adapter: Ecto.Adapters.Postgres,
-  url: System.get_env("DATABASE_URL"),
+  url: System.get_env("DATABASE_CONNECTION_POOL_URL") || System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
-  ssl: true
+  ssl: true,
+  prepare: :unnamed

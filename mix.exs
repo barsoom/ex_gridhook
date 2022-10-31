d7fbc418cef4a12b938220427ec36710d130e22e
Peter Wall <peter@auctionet.com>
2022-10-31 13:18:50 +0100

    chore: update elixir, erlang

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule ExGridhook.Mixfile do
       version: "0.0.1",
       elixir: ">= 0.0.0",
       elixirc_paths: elixirc_paths(Mix.env()),
-      compilers: [:phoenix, :gettext] ++ Mix.compilers(),
+      compilers: [:phoenix] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps()

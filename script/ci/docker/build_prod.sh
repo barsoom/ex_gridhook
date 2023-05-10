38d50d39034eb670b68651c25f7fe5d7ef77be03
Andrei Roman <claudiu.andrei.roman@icloud.com>
2023-05-10 12:03:17 +0200

    fix ci?
    Co-authored-by: Stefano Franzin <576548+ximes@users.noreply.github.com>

--- script/ci/docker/build_prod.sh
+++ script/ci/docker/build_prod.sh
@@ -3,8 +3,8 @@ set -e
 
 REVISION=$CIRCLE_SHA1 make prod-image
 
-docker tag ex-gridhook:web registry.heroku.com/auctionet-ex-gridhook/web:latest
+docker tag ${CIRCLE_PROJECT_REPONAME}:web registry.heroku.com/auctionet-ex-gridhook/web:latest
 
-web_image_id=`docker inspect ex-gridhook:web --format={{.Id}}`
+web_image_id=`docker inspect ${CIRCLE_PROJECT_REPONAME}:web --format={{.Id}}`
 
 echo "export WEB_IMAGE_ID=$web_image_id" >> $BASH_ENV

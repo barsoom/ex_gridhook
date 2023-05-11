178edc0237e83d2d4c129924ba9df18d88373a41
Andrei Roman <claudiu.andrei.roman@icloud.com>
2023-05-11 13:28:58 +0200

    Correct broken specs for reading the revision

--- test/ex_gridhook_web/controllers/root_controller_test.exs
+++ test/ex_gridhook_web/controllers/root_controller_test.exs
@@ -7,17 +7,28 @@ defmodule ExGridhookWeb.RootControllerTest do
     assert response(conn, 200) =~ "handles incoming sendgrid events"
   end
 
-  test "GET /revision without ENV set" do
+  test "GET /revision without built_from_revision file " do
+    File.rm!("built_from_revision")
+
     conn = get(build_conn(), "/revision")
     assert response(conn, 200) =~ "no revision is set."
   end
 
   test "GET /revision with ENV set" do
+    File.rm("built_from_revision")
+
     System.put_env("HEROKU_SLUG_COMMIT", "12345")
 
     conn = get(build_conn(), "/revision")
     assert response(conn, 200) == "12345"
 
     System.delete_env("HEROKU_SLUG_COMMIT")
+  end
+
+  test "GET /revision with file built_from_revision" do
+    File.write!("built_from_revision", "12345")
+
+    conn = get(build_conn(), "/revision")
+    assert response(conn, 200) == File.read!("built_from_revision")
   end
 end

00bc710acb3cb2fa4f4d1bc768776224584554dd
Peter Wall <peter@auctionet.com>
2023-05-11 08:16:11 +0200

    ci

--- script/ci/pipeline.sh
+++ script/ci/pipeline.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 # Script to report build status to pipeline (a dashboard for CI status)
 # See more at: https://github.com/barsoom/pipeline
 
@@ -8,25 +10,22 @@ command_to_run=$2
 
 # Shortcut to just run the build command if pipeline is not configured
 if [[ ! -v "PIPELINE_API_TOKEN" ]]; then
-  $command_to_run
-  exit $?
+  $command_to_run || exit 1
+  exit 0
 fi
 
-# Don't report non-master builds
+# Don't report non-default branch builds
 if [ "${CIRCLE_BRANCH}" != "master" ]; then
   $command_to_run || exit 1
   exit 0
 fi
 
-function _main {
+_main () {
   # Report start of build
   _post_build_status "building"
 
-  # Execute build command
-  $command_to_run
-
   # Report if the build succeeded of failed
-  if [[ $? == 0 ]]; then
+  if $command_to_run; then
     _post_build_status "successful"
   else
     _post_build_status "failed"
@@ -34,7 +33,7 @@ function _main {
   fi
 }
 
-function _post_build_status {
+_post_build_status () {
   curl --request POST "$PIPELINE_BASE_URL/api/build_status" --data "token=$PIPELINE_API_TOKEN&name=$build_name&repository=git@github.com:$CIRCLE_PROJECT_USERNAME/${CIRCLE_PROJECT_REPONAME}.git&revision=$CIRCLE_SHA1&status_url=https://circleci.com/gh/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/$CIRCLE_BUILD_NUM&status=$1"
 }
 
